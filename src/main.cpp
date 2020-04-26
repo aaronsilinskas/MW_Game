8743dcd47b5d0a44f443a15e7e5e7d23a483c2d0
Aaron Silinskas <aaron@mindwidgets.com>
2020-04-25 23:01:44 -0700

    main is now hacks to test combining magic

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,46 @@
-/* lamp.cpp Example sketch for a lamp
+/* Tool 3 prototype
  */
 #include <Arduino.h>
 #include <mw/time.h>
-#include <mw/magic.h>
 #include <mw/pixels.h>
-#include <mw/flame.h>
+#include <mw/magic.h>
 
-/////////////////////////// State
-
-
-/////////////////////////// Behaviors
-
-
-/////////////////////////// Entity
-
+// General
 Time time;
 
-Flame flame{
-    .lit = true,
-    .brightness = 0,
-    .burnedPerMs = 1};
+// Buttons
+#define PIN_BUTTON_FIRE A3
+#define PIN_BUTTON_WATER A4
+#define PIN_BUTTON_LIGHTING A5
 
-Magic fuel{
-    .type = Lightning,
-    .amount = 10000};
-
+// LEDs
 #define NEOPIXEL_PIN 5
 const uint16_t NEOPIXEL_COUNT = 12;
 CRGB neopixels[NEOPIXEL_COUNT];
 
-Pixels flamePixels{
-    .count = 1,
-    .colors = &neopixels[3]};
+Pixels pixels{
+    .count = 3,
+    .colors = &neopixels[0]};
+
+MagicType selectedMagic;
+MagicType castType;
+uint8_t magicSteps;
+
+#define ACTION_DELAY 750
+uint64_t nextActionMs;
+
+void setupButtons()
+{
+    pinMode(PIN_BUTTON_FIRE, INPUT_PULLUP);
+    pinMode(PIN_BUTTON_WATER, INPUT_PULLUP);
+    pinMode(PIN_BUTTON_LIGHTING, INPUT_PULLUP);
+}
+
+void setupMagic()
+{
+    magicSteps = 0;
+    nextActionMs = 0;
+}
 
 void setup()
 {
@@ -39,24 +48,116 @@ void setup()
 
     randomSeed(micros());
 
+    setupButtons();
     setupNeopixels<NEOPIXEL_PIN>(neopixels, NEOPIXEL_COUNT, true, 32);
+    setupMagic();
+}
+
+bool buttonPressed(uint32_t pin)
+{
+    return digitalRead(pin) == 0;
+}
+
+void updatePixelsForMagic(uint8_t steps, CRGB color)
+{
+    if (steps == 0)
+    {
+        for (int i = 0; i < pixels.count; i++)
+        {
+            pixels.colors[i] = color;
+        }
+        return;
+    }
+    else if (steps == 1)
+    {
+        pixels.colors[0] = color;
+    }
+    else if (steps == 2)
+    {
+        pixels.colors[0] = color;
+        pixels.colors[1] = color;
+    }
+    else
+    {
+        pixels.colors[2] = color;
+    }
+}
+
+void addMagic(MagicType magic)
+{
+    if (magicSteps == 0)
+    {
+        selectedMagic = magic;
+        updatePixelsForMagic(1, colorFromMagic(selectedMagic));
+        magicSteps++;
+    }
+    else if (magicSteps == 1)
+    {
+        Serial.print(magicTypeName(selectedMagic));
+        Serial.print(" + ");
+        Serial.print(magicTypeName(magic));
+        Serial.print(" = ");
+        Serial.println(magicTypeName(combineMagic(selectedMagic, magic)));
+
+        selectedMagic = combineMagic(selectedMagic, magic);
+        if (selectedMagic == None)
+        {
+            updatePixelsForMagic(0, CRGB::Black);
+            magicSteps = 0;
+        }
+        else
+        {
+            updatePixelsForMagic(2, colorFromMagic(selectedMagic));
+            magicSteps++;
+        }
+    }
+    else
+    {
+        castType = magic;
+        updatePixelsForMagic(3, colorFromMagic(castType));
+        magicSteps++;
+    }
+}
+
+bool spellComplete()
+{
+    return magicSteps >= 3;
+}
+
+void castSpell()
+{
+    // TODO cast the spell based on selectedMagic and castType
+
+    magicSteps = 0;
 }
 
 void loop()
 {
     timeEllapsed(&time);
-    flameBurnsMagic(&flame, &fuel, time);
-    flameBrightnessChanges(&flame, time);
-    CRGB flameColor = colorFromMagic(fuel);
-    flameOnPixel(flame, flameColor, &flamePixels, 0);
 
-    EVERY_N_MILLIS(750)
+    if (nextActionMs < time.currentTimeMs)
     {
-        printMagic(&Serial, "Fuel remaining", fuel);
+        if (buttonPressed(PIN_BUTTON_FIRE))
+        {
+            addMagic(Fire);
+            nextActionMs = time.currentTimeMs + ACTION_DELAY;
+        }
+        else if (buttonPressed(PIN_BUTTON_WATER))
+        {
+            addMagic(Water);
+            nextActionMs = time.currentTimeMs + ACTION_DELAY;
+        }
+        else if (buttonPressed(PIN_BUTTON_LIGHTING))
+        {
+            addMagic(Lightning);
+            nextActionMs = time.currentTimeMs + ACTION_DELAY;
+        }
+    }
+    if (spellComplete())
+    {
+        castSpell();
     }
 
     FastLED.show();
-
     FastLED.delay(20);
-    //FastLED.delay(750);
 }
